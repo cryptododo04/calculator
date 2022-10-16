bbf226d0f77bb8ea0a6dc4d79829f92ddd34b2c3
santi cuda <santiagoacuda@gmail.com>
2022-10-16 01:58:39 -0300

    Modified background gradient color

--- addStyles.js
+++ addStyles.js
@@ -1,4 +1,4 @@
-const bodyClass = "bg-purple-800 bg-gradient-to-br from-blue-300 bg-opacity-25";
+const bodyClass = "bg-green-700 bg-gradient-to-br from-blue-400 bg-opacity-25";
 const bdy = document.querySelector("body");
 
 bodyClass.split(" ").forEach((cls) => bdy.classList.add(cls));
